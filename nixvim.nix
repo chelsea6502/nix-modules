7df703c67cdd42c1316db99af1f15d1337036ceb
Chelsea Wilkinson <122856043+chelsea6502@users.noreply.github.com>
2025-09-22 15:09:22 +1000

    Disable Docker language server in nixvim.nix
    Removed Docker language server configuration to fix an error.

--- nixvim.nix
+++ nixvim.nix
@@ -168,8 +168,6 @@
     lsp.servers.nil_ls.settings.nix.flake.autoArchive = true;
     lsp.servers.nil_ls.settings.nix.flake.autoEvalInputs = true;
 
-    lsp.servers.dockerls.enable = false; # fix for an error
-
     telescope.enable = true;
     telescope.extensions.file-browser.enable = true;
 
